9fe87deb79f5fd9c633d1e76e427daadc891ec5f
Matan Tsuberi <tsuberim@gmail.com>
2025-08-23 18:46:30 +0300

    progress

--- train_vae.py
+++ train_vae.py
@@ -394,8 +394,8 @@ if __name__ == "__main__":
     trained_vae = train_vae(
         epochs=50,
         batch_size=21,
-        lr=5e-6,
-        beta=1e-7,  # Start with beta~=0 (no KL regularization)
+        lr=1e-5,
+        beta=1e-6,  # Start with beta~=0 (no KL regularization)
         latent_dim=16,
         num_frames=100_000,  # Use subset for faster training
         # visualize_every=1,  # Show reconstructions every epoch
