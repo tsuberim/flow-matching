a2b06c6973823f6fc8b692d99bfcd814d4754f4a
Matan Tsuberi <tsuberim@gmail.com>
2025-08-23 20:49:44 +0300

    progress

--- train_vae.py
+++ train_vae.py
@@ -373,7 +373,7 @@ if __name__ == "__main__":
     # Train VAE
     trained_vae = train_vae(
         epochs=50,
-        batch_size=10 * 32,
+        batch_size=8 * 32,
         lr=1e-5,
         beta=0.0,  # Start with beta~=0 (no KL regularization)
         latent_dim=16,
