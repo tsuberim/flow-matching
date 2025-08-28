8cf46c1f0f32fdcd3247a2223d58d9a11086851b
Matan Tsuberi <tsuberim@gmail.com>
2025-08-28 22:22:41 +0300

    progress

--- vae.py
+++ vae.py
@@ -182,7 +182,7 @@ class VideoVAE(nn.Module):
         return samples
 
 
-def vae_loss(vae, frames, beta=0.0, gamma=0.001):
+def vae_loss(vae, frames, beta=0.0, gamma=0.000):
     """
     VAE loss function
     Args:
